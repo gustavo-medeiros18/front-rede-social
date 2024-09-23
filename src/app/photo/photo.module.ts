e49bb18b187769c932c8b7301b755c1c5eb3c538
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-23 15:45:53 -0300

    ✨ feat: Adds PhotoModule with PhotoListaComponent declaration

---
+++ src/app/photo/photo.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhotoListaComponent } from './photo-lista/photo-lista.component';
+
+@NgModule({
+  declarations: [PhotoListaComponent],
+  imports: [CommonModule],
+})
+export class PhotoModule {}
