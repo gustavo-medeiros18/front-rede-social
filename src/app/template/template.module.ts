10695d9c869e1e64d26a881a7f452ea97cbdda94
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-23 14:11:57 -0300

    ✨ feat: Adds SidebarComponent to TemplateModule declarations.

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, SidebarComponent],
   imports: [CommonModule],
 })
 export class TemplateModule {}
