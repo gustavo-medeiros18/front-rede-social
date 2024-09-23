841a8dedbce41b37eee35db81d7872c515ff75fc
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-23 15:45:37 -0300

    ✨ feat: Adds PhotoListaComponent basic files.

---
+++ src/app/photo/photo-lista/photo-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-lista',
+  templateUrl: './photo-lista.component.html',
+  styleUrls: ['./photo-lista.component.css']
+})
+export class PhotoListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
