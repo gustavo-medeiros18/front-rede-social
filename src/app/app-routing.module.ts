39a8b69596cdd4c4233b95770e86a057fd8fce43
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-10-01 15:10:11 -0300

    ✨ feat: Adds home route and redirects to it in app routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TableComponent } from './table/table.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'photos-table', component: TableComponent }];
+const routes: Routes = [
+  { path: 'photos-table', component: TableComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
