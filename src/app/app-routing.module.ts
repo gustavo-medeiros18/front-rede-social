ef64970f6aaee82e4a2b262bc77e121c902e2ecc
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-10-02 09:20:50 -0300

    ✨ feat: Add user form route to app routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TableComponent } from './table/table.component';
 import { HomeComponent } from './home/home.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 const routes: Routes = [
   { path: 'photos-table', component: TableComponent },
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: 'user-form', component: UserFormComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
